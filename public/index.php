aa1f375439f5cd9bd5d378d59a1033f9cc83a5ca
daroui27 <darouidi@gmail.com>
2017-12-22 18:47:45 +0100

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -9,26 +9,26 @@ if (PHP_SAPI == 'cli-server') {
     }
 }
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '../vendor/autoload.php';
 spl_autoload_register(function ($classname) {
-    require (__DIR__ . '/../src/Models/' . $classname . ".php");
+    require (__DIR__ . '../src/Models/' . $classname . ".php");
 });
 
 
 session_start();
 
 // Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
+$settings = require __DIR__ . '../src/settings.php';
 $app = new \Slim\App($settings);
 
 // Set up dependencies
-require __DIR__ . '/../src/dependencies.php';
+require __DIR__ . '../src/dependencies.php';
 
 // Register middleware
-require __DIR__ . '/../src/middleware.php';
+require __DIR__ . '../src/middleware.php';
 
 // Register routes
-require __DIR__ . '/../src/routes.php';
+require __DIR__ . '../src/routes.php';
 
 // Run app
 $app->run();
